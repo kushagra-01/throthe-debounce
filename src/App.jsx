3cfe4613bb32a54d042a247e7fb493fcf0d5284d
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-28 01:57:08 +0530

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Debounce from "./debounce";
 import Throthle from "./throtthle";
 
@@ -5,11 +6,22 @@ import Throthle from "./throtthle";
 
 export default function App() {
   
+  const [text,settext]=useState("ascending")
+
+  const handle=(e)=>{
+if(text=="ascending"){
+  settext("descending")
+}
+else{
+  settext("ascending")
+}
+  }
 
   return (
     <div className="App">
      <Debounce/>
      <Throthle/>
+     <button onClick={((e)=>{handle(e.target.value)})}>{text}</button>
     </div>
   );
 }
